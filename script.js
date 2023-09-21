a67ebe075640ac285b4a44fd7fc6e4b6d1ffc7e1
Vaisakh-RS <vaisakhrs4@gmail.com>
2023-09-21 22:52:19 +0530

    Display the details of the book into the page

--- script.js
+++ script.js
@@ -9,10 +9,22 @@ function Book(number,name,author,readOrNot){
 
 };
 
-
 //adds newly created book into the myLibrary array
 function addBook(number,name,author,readOrNot){
    const book=new Book(number,name,author,readOrNot);
    myLibrary.push(book);
    return myLibrary
+}
+
+// addBook(20,"a","b",true);
+// addBook(40,"c","d",true);
+
+const booksDiv=document.getElementsByClassName("books")[0];
+
+for(let i=0;i<myLibrary.length;i++)
+{
+    console.log(myLibrary[i]);
+    let newDiv=document.createElement("div");
+    newDiv.textContent=myLibrary[i].number+" "+myLibrary[i].name+" "+myLibrary[i].author+" "+myLibrary[i].readOrNot;
+    booksDiv.appendChild(newDiv);
 }
